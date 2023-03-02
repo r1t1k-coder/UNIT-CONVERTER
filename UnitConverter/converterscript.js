350ec0936e10bf8d225af1b7eb6c8d364c5ba551
r1t1k-coder <80210710+r1t1k-coder@users.noreply.github.com>
2023-03-02 19:36:05 +0530

    Add files via upload

---
+++ UnitConverter/converterscript.js
@@ -0,0 +1,128 @@
+
+var property = new Array();
+var unit = new Array();
+var factor = new Array();
+
+property[0] = "Area";
+unit[0] = new Array("Square meter (m^2)", "Acre (acre)", "Are", "Barn (barn)", "Hectare", "Rood", "Square centimeter", "Square kilometer", "Circular mil", "Square foot (ft^2)", "Square inch (in^2)", "Square mile (mi^2)", "Square yard (yd^2)");
+factor[0] = new Array(1, 4046.856, 100, 1E-28, 10000, 1011.71413184285, .0001, 1000000, 5.067075E-10, 9.290304E-02, 6.4516E-04, 2589988, .8361274);
+
+property[1] = "Length";
+unit[1] = new Array("Meter (m)", "Angstrom (A')", "Astronomical unit (AU)", "Caliber (cal)", "Centimeter (cm)", "Kilometer (km)", "Ell", "Em", "Fathom", "Furlong", "Fermi (fm)", "Foot (ft)", "Inch (in)", "League (int'l)", "League (UK)", "Light year (LY)", "Micrometer (mu-m)", "Mil", "Millimeter (mm)", "Nanometer (nm)", "Mile (int'l nautical)", "Mile (UK nautical)", "Mile (US nautical)", "Mile (US statute)", "Parsec", "Pica (printer)", "Picometer (pm)", "Point (pt)", "Rod", "Yard (yd)");
+factor[1] = new Array(1, 1E-10, 1.49598E11, .000254, .01, 1000, 1.143, 4.2323E-03, 1.8288, 201.168, 1E-15, .3048, .0254, 5556, 5556, 9.46055E+15, .000001, .0000254, .001, 1E-9, 1852, 1853.184, 1852, 1609.344, 3.08374E+16, 4.217518E-03, 1E-12, .0003514598, 5.0292, .9144);
+
+property[2] = "Mass";
+unit[2] = new Array("Kilogram (kgr)", "Gram (gr)", "Milligram (mgr)", "Microgram (mu-gr)", "Carat (metric)(ct)", "Hundredweight (long)", "Hundredweight (short)", "Pound mass (lbm)", "Pound mass (troy)", "Ounce mass (ozm)", "Ounce mass (troy)", "Slug", "Ton (assay)", "Ton (long)", "Ton (short)", "Ton (metric)", "Tonne");
+factor[2] = new Array(1, .001, 1e-6, .000000001, .0002, 50.80235, 45.35924, .4535924, .3732417, .02834952, .03110348, 14.5939, .02916667, 1016.047, 907.1847, 1000, 1000);
+
+// !!! Caution: Temperature requires an increment as well as a multiplying factor
+// !!! and that's why it's handled differently
+
+property[3] = "Temperature";
+unit[3] = new Array("Degrees Celsius ('C)", "Degrees Fahrenheit ('F)", "Degrees Kelvin ('K)", "Degrees Rankine ('R)");
+factor[3] = new Array(1, 0.555555555555, 1, 0.555555555555);
+tempIncrement = new Array(0, -32, -273.15, -491.67);
+
+property[4] = "Time";
+unit[4] = new Array("Second (sec)", "Day (mean solar)", "Day (sidereal)", "Hour (mean solar)", "Hour (sidereal)", "Minute (mean solar)", "Minute (sidereal)", "Month (mean calendar)", "Second (sidereal)", "Year (calendar)", "Year (tropical)", "Year (sidereal)");
+factor[4] = new Array(1, 8.640E4, 86164.09, 3600, 3590.17, 60, 60, 2628000, .9972696, 31536000, 31556930, 31558150);
+
+
+property[5] = "Volume & Capacity";
+unit[5] = new Array("Cubic Meter (m^3)", "Cubic centimeter", "Cubic millimeter", "Acre-foot", "Barrel (oil)", "Board foot", "Bushel (US)", "Cup", "Fluid ounce (US)", "Cubic foot", "Gallon (UK)", "Gallon (US,dry)", "Gallon (US,liq)", "Gill (UK)", "Gill (US)", "Cubic inch (in^3)", "Liter (new)", "Liter (old)", "Ounce (UK,fluid)", "Ounce (US,fluid)", "Peck (US)", "Pint (US,dry)", "Pint (US,liq)", "Quart (US,dry)", "Quart (US,liq)", "Stere", "Tablespoon", "Teaspoon", "Ton (register)", "Cubic yard");
+factor[5] = new Array(1, .000001, .000000001, 1233.482, .1589873, .002359737, .03523907, .0002365882, .00002957353, .02831685, .004546087, .004404884, .003785412, .0001420652, .0001182941, .00001638706, .001, .001000028, .00002841305, .00002957353, 8.8097680E-03, .0005506105, 4.7317650E-04, .001101221, 9.46353E-04, 1, .00001478676, .000004928922, 2.831685, .7645549);
+
+// ===========
+//  Functions
+// ===========
+
+function UpdateUnitMenu(propMenu, unitMenu) {
+  // Updates the units displayed in the unitMenu according to the selection of property in the propMenu.
+  var i;
+  i = propMenu.selectedIndex;
+  FillMenuWithArray(unitMenu, unit[i]);
+}
+
+function FillMenuWithArray(myMenu, myArray) {
+  // Fills the options of myMenu with the elements of myArray.
+  // !CAUTION!: It replaces the elements, so old ones will be deleted.
+  var i;
+  myMenu.length = myArray.length;
+  for (i = 0; i < myArray.length; i++) {
+    myMenu.options[i].text = myArray[i];
+  }
+}
+
+function CalculateUnit(sourceForm, targetForm) {
+  // A simple wrapper function to validate input before making the conversion
+  var sourceValue = sourceForm.unit_input.value;
+
+  // First check if the user has given numbers or anything that can be made to one...
+  sourceValue = parseFloat(sourceValue);
+  if (!isNaN(sourceValue) || sourceValue == 0) {
+    // If we can make a valid floating-point number, put it in the text box and convert!
+    sourceForm.unit_input.value = sourceValue;
+    ConvertFromTo(sourceForm, targetForm);
+  }
+}
+
+function ConvertFromTo(sourceForm, targetForm) {
+  // Converts the contents of the sourceForm input box to the units specified in the targetForm unit menu and puts the result in the targetForm input box.In other words, this is the heart of the whole script...
+  var propIndex;
+  var sourceIndex;
+  var sourceFactor;
+  var targetIndex;
+  var targetFactor;
+  var result;
+
+  propIndex = document.property_form.the_menu.selectedIndex;
+
+  sourceIndex = sourceForm.unit_menu.selectedIndex;
+  sourceFactor = factor[propIndex][sourceIndex];
+
+  //for the target unit - the units we are converting TO:
+  targetIndex = targetForm.unit_menu.selectedIndex;
+  targetFactor = factor[propIndex][targetIndex];
+
+  // math: a) convert the source TO the base unit: (The input has been checked by the CalculateUnit function).
+
+  result = sourceForm.unit_input.value;
+  // Handle Temperature increments!
+  if (property[propIndex] == "Temperature") {
+    result = parseFloat(result) + tempIncrement[sourceIndex];
+  }
+  result = result * sourceFactor;
+
+  // b) use the targetFactor to convert FROM the base unit
+  // to the target unit...
+  result = result / targetFactor;
+  // Again, handle Temperature increments!
+  if (property[propIndex] == "Temperature") {
+    result = parseFloat(result) - tempIncrement[targetIndex];
+  }
+
+  // All that's left is to update the target input box:
+  targetForm.unit_input.value = result;
+}
+
+// This fragment initializes the property dropdown menu using the data defined above in the 'Data Definitions' section
+window.onload = function(e) {
+  FillMenuWithArray(document.property_form.the_menu, property);
+  UpdateUnitMenu(document.property_form.the_menu, document.form_A.unit_menu);
+  UpdateUnitMenu(document.property_form.the_menu, document.form_B.unit_menu)
+}
+
+// Restricting textboxes to accept numbers + navigational keys only
+document.getElementByClass('numbersonly').addEventListener('keydown', function(e) {
+  var key = e.keyCode ? e.keyCode : e.which;
+
+  if (!([8, 9, 13, 27, 46, 110, 190].indexOf(key) !== -1 ||
+      (key == 65 && (e.ctrlKey || e.metaKey)) || // Select All
+      (key == 67 && (e.ctrlKey || e.metaKey)) || // Copy
+      (key == 86 && (e.ctrlKey || e.metaKey)) || // Paste
+      (key >= 35 && key <= 40) || // End, Home, Arrows
+      (key >= 48 && key <= 57 && !(e.shiftKey || e.altKey)) || // Numeric Keys
+      (key >= 96 && key <= 105) // Numpad
+      (key == 190) // Numpad
+    )) e.preventDefault();
+});
